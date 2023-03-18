49df97e952aa0b775272211650352c03e78add47
Ragul R <ragul542rv@gmail.com>
2023-03-18 22:03:44 +0530

    delete user permission endpoint

--- api/dlapp/history.py
+++ api/dlapp/history.py
@@ -4,23 +4,23 @@ from sqlalchemy import select, delete
 from ..schemas import Role
 
 from .models import Permission as PermissionDB
-from .schemas import Designation, Status
+from .schemas import Designation, DeanStatus, HodStatus
 
 
 def get_history(head, user, db):
     if head and (Role.DEAN in user.roles or Role.HR in user.roles):
-        stmt = select(PermissionDB).where(PermissionDB.dean_status != Status.PENDING)
+        stmt = select(PermissionDB).where(
+            PermissionDB.dean_status != DeanStatus.PENDING
+        )
         admin_history = db.scalars(stmt).all()
         return admin_history
 
     elif head:
-        group = next(
-            group for group in user.groups if Designation.HEADS.value in group.lower()
-        )
+        group = next(group for group in user.groups if Designation.HEADS.value in group)
         department = group.split("/")[2]
         stmt = select(PermissionDB).where(
             PermissionDB.user_group.in_(department)
-            and PermissionDB.hod_status != Status.PENDING
+            and PermissionDB.hod_status != HodStatus.PENDING
         )
         admin_history = db.scalars(stmt).all()
         return admin_history
@@ -34,10 +34,33 @@ def get_history(head, user, db):
 def delete_permission(permission_id, user, db):
     if Role.HR not in user.roles:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-    else:
-        stmt = delete(PermissionDB).where(PermissionDB.c.permission_id == permission_id)
+
+    stmt = select(PermissionDB.dean_status).where(
+        PermissionDB.permission_id == permission_id
+    )
+    dean_status = db.scalars(stmt).first()
+
+    if dean_status != DeanStatus.APPROVED:
+        stmt = delete(PermissionDB).where(PermissionDB.permission_id == permission_id)
         db.execute(stmt)
         db.commit()
         return True
 
+    return False
+
+
+def delete_user_permission(permission_id, user, db):
+    if Role.STAFF not in user.roles:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    stmt = select(PermissionDB.dean_status).where(
+        PermissionDB.permission_id == permission_id
+    )
+    dean_status = db.scalars(stmt).first()
+
+    if dean_status == DeanStatus.PENDING:
+        stmt = delete(PermissionDB).where(PermissionDB.permission_id == permission_id)
+        db.execute(stmt)
+        db.commit()
+        return True
     return False
